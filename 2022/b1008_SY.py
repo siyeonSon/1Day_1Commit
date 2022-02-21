bbd5b856f0c9a8dec5d5aaa8d6761c3e894f796c
siyeonSon <siyeon0209@naver.com>
2022-02-21 16:17:16 +0900

    20220221_SY

---
+++ 2022/b1008_SY.py
@@ -0,0 +1,3 @@
+import sys
+N = list(map(int, sys.stdin.readline().split()))
+print(N[0] / N[1])
---
+++ 2022/b2525_SY.py
@@ -0,0 +1,12 @@
+import sys
+A, B = map(int, sys.stdin.readline().split())
+C = int(input())
+
+A = A + C//60
+B = B + C % 60
+if B >= 60:
+    A = A + 1
+    B = B - 60
+if A >= 24:
+    A = A - 24
+print(A, B)
