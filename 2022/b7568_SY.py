b943eba2aba5315fe550bfee65eb098ccb82c86b
siyeonSon <siyeon0209@naver.com>
2022-04-08 00:00:26 +0900

    20220407_SY

--- 2022/b7568_SY.py
+++ 2022/b7568_SY.py
@@ -1,46 +1,17 @@
-def element_ranking(list):
-    n = len(list)
-    score = [0] * n
-    for i in range(n-1):
-        for j in range(i+1, n):
-            if list[i] > list[j]:
-                score[i] += 1
-            elif list[i] < list[j]:
-                score[j] += 1
-            else:
-                score[i] += 0.5
-                score[j] += 0.5
-    return score
-
-
 n = int(input())
-answer = [0] * n
-
-xList, yList = [], []
-list = [i for i in range(n)]
+x_y = []
 for _ in range(n):
-    x, y = map(int, input().split())
-    xList.append(x)
-    yList.append(y)
+    x_y.append(list(map(int, input().split())))
 
-rank = [xR+yR for xR,
-        yR in zip(element_ranking(xList), element_ranking(yList))]
-
-rank_num = []
+rank = [0] * n
 for i in range(n):
-    rank_num.append([rank[i], list[i]])
-rank_num.sort(reverse=True)
-# print(rank_num)
+    for j in range(n):
+        if x_y[i][0] < x_y[j][0] and x_y[i][1] < x_y[j][1]:
+            rank[i] += 1  # 자신보다 덩치가 작은 사람의 수
 
-tmp = [0, 0]  # [ranking하여 얻은 값, 등수]
-for i in range(1, n+1):
-    num = rank_num.pop(0)
-    # print(num)
-    if tmp[0] == num[0]:
-        answer[num[1]] = tmp[1]
-    else:
-        answer[num[1]] = i
-        tmp = [num[0], i]
+# 덩치 등수는 자신보다 덩치가 작은 사람의 수 + 1 이다
+for r in rank:
+    print(r+1, end=" ")
 
-for a in answer:
-    print(a, end=" ")
+
+# 등수는 나보다 뛰어난 사람이 위에 몇 명 있는지를 나타내는 것!
