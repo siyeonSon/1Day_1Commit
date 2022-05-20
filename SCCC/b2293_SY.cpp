5136e2391420dc2f4057273bd1a0083c3a098d0d
siyeonSon <siyeon0209@naver.com>
2022-05-20 15:24:51 +0900

    20220520_SY

---
+++ SCCC/b2293_SY.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k, tmp;
+    cin >> n >> k;
+
+    //dp[i] = i원을 만들 경우
+    //dp[0] = 0원을 만들 경우 = 동전을 선택하지 않는 경우
+    int dp[k+1] = {1};
+
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        for (int j = 0; j <= k-tmp; j++) {
+            dp[j+tmp] += dp[j];
+        }
+    }
+
+    cout << dp[k];
+
+    return 0;
+}
